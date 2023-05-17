e248ab02463e285a8869f00eb1b746f513cd9db6
rhaniel99 <rhanielmonteiro.13@gmail.com>
2023-05-17 16:51:15 -0400

    1 commits

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require('express');
 const app = express();
 
 // Rota para o redirecionamento
-app.get('/redirecionar', (req, res) => {
-  const originAPI = req.query.api || 'API-PORT-4000';
-
-  // Redireciona para a URL da API A na porta 3000
-  res.redirect(`http://localhost:4000/api/user/retorno?originAPI=${originAPI}`);
+app.get('/redirect', (req, res) => {
+  const returnAPI = 'http://localhost:4000'; // URL da API na porta 4000
+  const originAPI = req.query.api || 'API-3000';
+  // Redireciona para a URL da API na porta 4000
+  res.redirect(`${returnAPI}/api/user/retorno?originAPI=${originAPI}`);
 });
 
-// Inicia o servidor na porta 4000
+// Inicia o servidor na porta 3000
 app.listen(3000, () => {
-  console.log('API Final iniciada na porta 3000');
+  console.log('Cliente iniciado na porta 3000');
 });

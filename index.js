f7f69dfadf18643ef2938292f209f75a037bf546
rhaniel99 <rhanielmonteiro.13@gmail.com>
2023-05-18 14:39:00 -0400

    cliente ok

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ app.get('/redirect', (req, res) => {
   res.redirect(`${returnAPI}/api/user/retorno?originAPI=${originAPI}`);
 });
 
+app.get('/api/user/retorno', (req, res) => {
+  // Obtenha o cabeçalho personalizado da resposta
+  const accessingAPI = req.header('X-Accessing-API');
+  
+  // Resto da lógica para lidar com a resposta do servidor
+  // ...
+  
+  // Exemplo de exibição no console
+  console.log(`Acesso à API: ${accessingAPI}`);
+  
+  res.send('Resposta recebida do servidor');
+});
+
 // Inicia o servidor na porta 3000
 app.listen(3000, () => {
   console.log('Cliente iniciado na porta 3000');

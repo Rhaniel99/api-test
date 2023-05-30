555eda81bc9df58d41281a0d1d3df6d275067e52
rhaniel99 <rhanielmonteiro.13@gmail.com>
2023-05-30 16:59:58 -0400

    fetch troca

--- index.js
+++ index.js
@@ -3,31 +3,75 @@ const axios = require("axios");
 const app = express();
 app.use(express.static("./public"));
 
-// Rota para o redirecionamento
-app.get("/callback", (req, res) => {
-  const returnAPI = "http://localhost:4000"; // URL da API na porta 4000
-  const originAPI = req.query.api || "API-3500";
+app.get("/login", (req, res) => {
+  const authorizationServer = "http://localhost:4000"; // URL do servidor OAuth 2.0
+  const clientId = "1"; // Substituir pelo ID do cliente
+  const redirectUri = "http://localhost:3500/oauth/seduc/callback"; // URL de redirecionamento do cliente
 
-  // Configuração dos cabeçalhos
-  const headers = {
-    authorization:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Im5hbWUiOiJDJ2VzdCBsYSB2aWUgQVBJIiwidXJsRG9tYWluIjoiQVBJLTM1MDAiLCJ0b2tlbiI6IjFkNGIxYTVmLTAzNzUtNGYyNC05OWYwLTAzNmQ3ZDhhMWM3OCJ9LCJpYXQiOjE2ODUxMTY2NjB9.jkCU7bykrbQ1jVXQD2cuAQXnrvtV-_-S73EKAn7bOkg",
-    "Content-Type": "application/json",
-  };
+  const authorizeUrl = `${authorizationServer}/api/user/authorize?client_id=${clientId}&redirect_uri=${redirectUri}`;
 
-  // Envia uma solicitação GET usando o Axios
-  axios
-    .get(`${returnAPI}/api/user/callback?originAPI=${originAPI}`, { headers })
-    .then((response) => {
-      res.send(response.data);
+  // Redirecionar o usuário para a rota /authorize do servidor OAuth 2.0
+  fetch(authorizeUrl)
+    .then(response => response.json())
+    .then(data => {
+      res.json(data);
     })
-    .catch((error) => {
+    .catch(error => {
       console.error(error);
-      res.status(500).send("Erro ao fazer a solicitação");
+      res.status(500).send("Erro na autenticação");
     });
 });
 
-// Inicia o servidor na porta 3000
+// Rota para o redirecionamento
+app.get("/oauth/seduc/callback", (req, res) => {
+  const authorizationServer = "http://localhost:4000"; // URL do servidor OAuth 2.0
+  const clientId = "1"; // Substituir pelo ID do cliente
+  const clientSecret = "123"; // Substituir pelo segredo do cliente
+  const redirectUri = "http://localhost:3500/oauth/seduc/callback"; // URL de redirecionamento do cliente
+
+  // Trocar o código de autorização por um token de acesso
+  const tokenEndpoint = `${authorizationServer}/api/user/callback`;
+  const params = {
+    code: req.query.code,
+    client_id: clientId,
+    client_secret: clientSecret,
+    redirect_uri: redirectUri,
+    grant_type: "authorization_code",
+  };
+
+  fetch(tokenEndpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(params),
+  })
+    .then(response => response.json())
+    .then(data => {
+      const accessToken = data.access_token;
+      console.log(accessToken);
+      // Utilizar o token de acesso para autenticar as solicitações para a API protegida
+      // ...
+      fetch(`${authorizationServer}/api/user/protegida`, {
+        headers: { authorization: `Bearer ${accessToken}` },
+      })
+        .then(apiResponse => apiResponse.json())
+        .then(data => {
+          const redirectUrl = data.redirectUrl;
+          res.json({ redirectUrl });
+        })
+        .catch(error => {
+          console.error(error);
+          res.status(500).send("Erro na autenticação");
+        });
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send("Erro na autenticação");
+    });
+});
+
+// Inicia o servidor na porta 3500
 app.listen(3500, () => {
   console.log("Cliente iniciado na porta 3500");
 });

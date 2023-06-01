1d9dd09815550fe341add5b31f04bb9fd9b61b45
rhaniel99 <rhanielmonteiro.13@gmail.com>
2023-06-01 15:45:27 -0400

    tudo funcionand no lado cliente

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ const express = require("express");
 const app = express();
 app.use(express.static("./public"));
 
-
 // Rota para o redirecionamento
 app.get("/oauth/seduc/callback", (req, res) => {
   const authorizationServer = "http://localhost:4000"; // URL do servidor OAuth 2.0
   const clientId = "1"; // Substituir pelo ID do cliente
-  const clientSecret = "123"; // Substituir pelo segredo do cliente
+  const clientSecret = "SECRET"; // Substituir pelo segredo do cliente
   const redirectUri = "http://localhost:3500/oauth/seduc/callback"; // URL de redirecionamento do cliente
 
-
-  
   // Trocar o código de autorização por um token de acesso
   const tokenEndpoint = `${authorizationServer}/api/user/callback`;
   const params = {
@@ -31,24 +28,7 @@ app.get("/oauth/seduc/callback", (req, res) => {
     .then((response) => response.json())
     .then((data) => {
       const accessToken = data.access_token;
-      fetch(`${authorizationServer}/api/user/protegida`, {
-        headers: { authorization: `Bearer ${accessToken}` },
-      })
-      .then((apiResponse) => {
-        if (apiResponse.ok) {
-          // A requisição para a API protegida foi bem-sucedida
-          // Redirecione o usuário para outra página
-          console.log(apiResponse);
-          res.send("ok");
-        } else {
-          // A requisição para a API protegida falhou
-          // Faça algo para lidar com o erro
-        }
-      })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send("Erro na autenticação");
-        });
+      res.redirect(`/redirect?token=${accessToken}`);
     })
     .catch((error) => {
       console.error(error);
@@ -56,6 +36,10 @@ app.get("/oauth/seduc/callback", (req, res) => {
     });
 });
 
+app.get("/redirect", (req, res) => {
+  const accessToken = req.query.token;
+  res.redirect(`http://localhost:4000/api/user/protegida?token=${accessToken}`);
+});
 
 app.listen(3500, () => {
   console.log("Cliente iniciado na porta 3500");
